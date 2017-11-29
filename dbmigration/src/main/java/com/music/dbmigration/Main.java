fe383be53c6f5b18ce36c15fb18fd789201925c3
Gede_Hermawan <gedehermawan2606@gmail.com>
2017-11-29 19:08:26 +0700

    testing database

--- dbmigration/src/main/java/com/music/dbmigration/Main.java
+++ dbmigration/src/main/java/com/music/dbmigration/Main.java
@@ -1,5 +1,6 @@
 package com.music.dbmigration;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
 //@Import(FlywayConfiguration.class)
@@ -19,10 +22,26 @@ public class Main {
     SpringApplication.run(Main.class, args);
   }
 
+//  @Bean
+//  @ConfigurationProperties(prefix = "database")
+//  @Lazy
+//  public DataSource dataSource() {
+//    return DataSourceBuilder.create().build();
+//  }
+
   @Bean
-  @ConfigurationProperties(prefix = "database")
-  @Lazy
-  public DataSource dataSource() {
-    return DataSourceBuilder.create().build();
+  public BasicDataSource dataSource() throws URISyntaxException {
+    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+    BasicDataSource basicDataSource = new BasicDataSource();
+    basicDataSource.setUrl(dbUrl);
+    basicDataSource.setUsername(username);
+    basicDataSource.setPassword(password);
+
+    return basicDataSource;
   }
 }
